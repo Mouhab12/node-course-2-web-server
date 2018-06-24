b0a3fbb1e939edd475ed8f5e8d47bcde51a21e00
Mouhab12 <mouhabelsakran@outlook.com>
2018-06-24 04:25:59 +0200

    portfolio added

--- server.js
+++ server.js
@@ -51,6 +51,14 @@ app.get('/about', (req, res) =>{
   });
 });
 
+app.get('/projects', (req, res) =>{
+  res.render('projects.hbs', {
+    pageTitle : 'Projects Page',
+    welcomeMessage : 'Portfolio page here'
+  });
+});
+
+
 app.get('/bad', (req, res) =>{
   res.send({
     errorMessage : 'Unable to handle request !'
